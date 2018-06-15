dde163c5564d3cde45b4fe80755a91cb87465a94
Nahuel Sotosca <nahuelsotosca@gmail.com>
2018-06-14 21:07:32 -0300

    ok ejercicio 6

--- Clase 06/Alumno/Ejercicio/index.js
+++ Clase 06/Alumno/Ejercicio/index.js
@@ -1,6 +1,12 @@
 function deleteElement(index, numbers) {
-  numbers.splice(index, 1)
-  return numbers.join('-')
+  if (Array.isArray(numbers)) {
+    numbers.splice(index, 1)
+    var numbersCopy = []
+    numbersCopy = numbers.slice()
+  } else {
+    return 'No es array'
+  }
+  return numbersCopy.join('-')
 }
 var numbers = [1, 2, 3, 4, 5]
 console.log(deleteElement(3, numbers))
