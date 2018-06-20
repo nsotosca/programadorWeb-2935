c63a94bdcb76457cb33d80c3b61384244cf9452c
Nahuel Sotosca <nahuelsotosca@gmail.com>
2018-06-19 21:09:18 -0300

    ok desafio 7

--- Clase 07/Alumno/Desafío/index.js
+++ Clase 07/Alumno/Desafío/index.js
@@ -26,5 +26,19 @@ function saveLocalList (array) {
     localStorage.setItem('list', array)
   }
 }
-
 saveLocalList(studentsList)
+
+function getLocalList (key) {
+  if (key) {
+    var array = localStorage.getItem(key)
+    array = JSON.parse(array)
+    if (Array.isArray(array)) {
+      return array
+    } else {
+      var array = []
+      return array
+    }
+  } else return (array = [])
+}
+
+console.log(getLocalList('list'))
